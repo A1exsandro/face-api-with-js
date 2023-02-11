d9b7cc86d77da170ac382a0a6e0c15689307a4c7
A1exsandro <alexsandro.acp@gmail.com>
2023-02-10 23:37:09 -0400

    refactor

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,45 +3,98 @@ const cam = document.getElementById('cam')
 const startVideo = () => {
   navigator.mediaDevices.enumerateDevices()
   .then(devices => {
-    devices.forEach(device => {
-      if (device.kind === 'videoinput') {
-        navigator.getUserMedia(
-          {
-            video: {
-              deviceId: device.deviceId
-            }
-          },
-          stream => cam.srcObject = stream,
-          error => console.log(error)
-        )
-      }
-    })
+    if (Array.isArray(devices)) {
+      devices.forEach(device => {
+        if (device.kind === 'videoinput') {
+          navigator.getUserMedia(
+            { video: {
+                deviceId: device.deviceId
+            }},
+            stream => cam.srcObject = stream,
+            error => console.error(error)
+          )
+        }
+      })
+    }
   })
 }
 
+const loadLabels = () => {
+  const labels = ['Alexsandro', 'Adriana', 'Kemilly', 'Alexia']
+  return Promise.all(labels.map(async label => {
+    const descriptions = []
+    for (let i = 1; i <= 1; i++) {
+      const img = await faceapi.fetchImage(`/assets/lib/face-api/labels/${label}/${i}.jpg`)
+      const detection = await faceapi
+        .detectSingleFace(img)
+        .withFaceLandmarks()
+        .withFaceDescriptor()
+
+        if (!detection) {
+          throw new Error(`no faces detected for ${label}`)
+        }
+      descriptions.push(detection.descriptor)
+
+      console.log(detection.descriptor)
+      console.log(detection)
+  
+    }
+      
+    return new faceapi.LabeledFaceDescriptors(label, descriptions)
+      
+  }))
+}
+
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/assets/lib/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/assets/lib/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/assets/lib/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/assets/lib/models'),
-  faceapi.nets.ageGenderNet.loadFromUri('/assets/lib/models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('/assets/lib/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('/assets/lib/face-api/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/assets/lib/face-api/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/assets/lib/face-api/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/assets/lib/face-api/models'),
+  faceapi.nets.ageGenderNet.loadFromUri('/assets/lib/face-api/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/assets/lib/face-api/models'),
 ]).then(startVideo)
 
-// DRAW THE CANVAS AND DETECT
 cam.addEventListener('play', async () => {
   const canvas = faceapi.createCanvasFromMedia(cam)
   const canvasSize = {
-    width: cam.width,
-    height: cam.height
+      width: cam.width,
+      height: cam.height
   }
+  const labels = await loadLabels()
   faceapi.matchDimensions(canvas, canvasSize)
   document.body.appendChild(canvas)
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(cam, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
-    const resizeDetections = await faceapi.resizeResults(detections, canvasSize)
-    canvas.getContext('2d').clearRect(0,0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizeDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizeDetections)
+    const detection = await faceapi
+      .detectAllFaces(
+        cam,
+        new faceapi.TinyFaceDetectorOptions()
+      )
+      .withFaceLandmarks()
+      .withFaceExpressions()
+      .withAgeAndGender()
+      .withFaceDescriptors()
+    const resizedDetections = faceapi.resizeResults(detection, canvasSize)
+    const faceMatcher = new faceapi.FaceMatcher(labels, 0.6)
+    const results = resizedDetections.map(d =>
+      faceMatcher.findBestMatch(d.descriptor)
+    )
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    resizedDetections.forEach(detection => {
+      const { age, gender, genderProbability } = detection
+      new faceapi.draw.DrawTextField([
+        `${parseInt(age, 10)} years`,
+        `${gender} (${parseInt(genderProbability * 100, 10)})`
+      ], detection.detection.box.topRight).draw(canvas)
+    })
+    results.forEach((result, index) => {
+      const box = resizedDetections[index].detection.box
+      const { label, distance } = result
+      new faceapi.draw.DrawTextField([
+        `${label} (${parseInt(distance * 100, 10)})`
+      ], box.bottomRight).draw(canvas)
+    })
   }, 100)
 })
